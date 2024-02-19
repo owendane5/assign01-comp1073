62e2192760783a13e80800cb0159cfdd2229614b
owendane5 <112992614+owendane5@users.noreply.github.com>
2024-02-18 20:18:09 -0500

    More changes and first scripting part

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+//My information
+const firstChoices = ['The Teacher', 'The Neighbor', 'The Fish', 'I', 'The Tiger'];
+const secondChoices = ['Ran', 'Swam', 'Leaped', 'Cried', 'Flew'];
+const thirdChoices = ['Up a Mountain', 'Under the Water', 'During the Test', 'While Eating', 'In the Desert'];
+const fourthChoices = ["In the Summer", "In the Winter", "In the Fall", "In the Spring", "In the Past"];
+const fifthChoice = ["On a Boat", "On a Bed", "On a Car", "On a Hill", "On a Roof"];
+//Idea is to store last index to be able to print out the choices at the end
+let lastSelectedOne = '';
+let lastSelectedTwo = '';
+let lastSelectedThree = '';
+let lastSelectedFour = '';
+let lastSelectedFive = '';
+//To keep track of what the user is currently on
+let choiceOneIndex = 0;
+let choiceTwoIndex = 0;
+let choiceThreeIndex = 0;
+let choiceFourIndex = 0;
+let choiceFiveIndex = 0;
+
+
+
